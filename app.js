8323b4c98eb94330275f23a7049834d91c328fbb
chakravarthi gadiparthi <31787401+gadiparthichakravarthi@users.noreply.github.com>
2018-01-14 16:16:24 +0000

    adding the event function with enter keyword

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 var main = function(){
 	"use strict";
-	$(".comment-input button").on("click" ,function(event){
-	var $new_comment = $("<p>").text($(".comment-input input").val());
-	
+	var addcommentfrominptubox = function() {
+		var $new_comment;
+		if($(".comment-input input").val() !== ""){
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$new_comment.hide();
+			$(".comments").append($new_comment);
+			$new_comment.fadeIn();
+			$(".comment-input input").val("");
+		}
+	};
 
-	$(".comments").append($new_comment);
-	});	
+	$(".comment-input button").on("click" ,function(event){
+	addcommentfrominptubox();
+
+	});
+	$(".comment-input input").on("keypress" , function(event){
+		if (event.keyCode === 13){
+			addcommentfrominptubox();
+		}
 	});
 };
 $(document).ready(main);
